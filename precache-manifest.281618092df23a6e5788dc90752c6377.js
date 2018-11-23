a50b2c4e7153c7083d698c7a5d249ea0d1193b8a
Roo Williams <roo@roowilliams.com>
2018-11-23 12:29:50 -0500

    Updates

---
+++ precache-manifest.281618092df23a6e5788dc90752c6377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a111364dba8d781ea25c843815ecc19",
+    "url": "/static/media/flowers.0a111364.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66886677a8618c3341cc",
+    "url": "/static/js/main.66886677.chunk.js"
+  },
+  {
+    "revision": "49d0cb0b0879268071e7",
+    "url": "/static/js/1.49d0cb0b.chunk.js"
+  },
+  {
+    "revision": "66886677a8618c3341cc",
+    "url": "/static/css/main.8049e6a1.chunk.css"
+  },
+  {
+    "revision": "f8c82dc8f309dc21bf71c0304c212e17",
+    "url": "/index.html"
+  }
+];
